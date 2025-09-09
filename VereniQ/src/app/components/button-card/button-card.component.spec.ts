913c2e290ee5d0531feb38484754862b3ac7e7a5
Gideon Dijkhuis <dijkhuis.g.a@outlook.com>
2025-09-09 21:48:09 +0200

    feat: Added Button-Card

---
+++ VereniQ/src/app/components/button-card/button-card.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { ButtonCardComponent } from './button-card.component';
+
+describe('ButtonCardComponent', () => {
+  let component: ButtonCardComponent;
+  let fixture: ComponentFixture<ButtonCardComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [ButtonCardComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ButtonCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
